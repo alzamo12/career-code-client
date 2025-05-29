99cecbc59f7749f31797d06625b04d44ce97289a
alzamo12 <rafiqulislam4969@gmail.com>
2025-05-29 21:58:59 +0600

    basice project setup is done

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="card-title">This is Navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import Navbar from "../Pages/Shared/Navbar";
+import Footer from "../Pages/Shared/Footer";
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default RootLayout;
